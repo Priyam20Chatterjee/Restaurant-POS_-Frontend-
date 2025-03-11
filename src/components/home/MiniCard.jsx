7424ca8209eb1465ee9afd54ac6ff6d1124a6f30
Priyam20Chatterjee <chatterjeepriyam96@gmail.com>
2025-03-12 01:20:12 +0530

    Mini Card Component created

--- src/components/home/MiniCard.jsx
+++ src/components/home/MiniCard.jsx
@@ -1,8 +1,18 @@
 // import React from 'react'
 
-const MiniCard = () => {
+// eslint-disable-next-line react/prop-types
+const MiniCard = ({title, number, icon, footerNum}) => {
     return (
-      <div>MiniCard</div>
+      <div className="bg-zinc-700 py-5 px-5 rounded-lg w-[50%]">
+          <div className="flex justify-between items-start">
+              <h1 className="text-white text-lg font-semibold tracking-wide">{title}</h1>
+              <button className={`${title === "Total Earnings" ? "bg-[#02ca3a]":"bg-[#f6B100]"} p-3 rounded-lg text-white text-2xl`}>{icon}</button>
+          </div>
+          <div>
+              <h1 className="text-white text-3xl font-bold mt-3">{number}</h1>
+              <h1 className="text-white text-lg mt-2"><span className="text-[#02ca3a]">{footerNum}%</span> than Yesterday</h1>
+          </div>
+      </div>
     );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 // import React from 'react'
 import BottomNav from "../components/shared/BottomNav";
 import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
 const Home = () => {
   return (
     <section className="bg-zinc-800 h-[calc(100vh-5rem)] overflow-hidden flex gap-3 ">
       <div className="flex-[3]">
         <Greetings />
+
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard title="Total Earnings" icon={< BsCashCoin/>} number={512} footerNum={1.6}/>
+          <MiniCard title="Total Earnings" icon={< GrInProgress/>} number={16} footerNum={3.6}/>
+        </div>
       </div>
 
       <div className="flex-[2] bg-blue-400"></div>
